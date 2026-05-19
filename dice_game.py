6890070c578e39416e070410c4255477cf3fdbfa
Jeremy Eldredge <jeldred1@byu.edu>
2026-05-19 12:14:40 -0600

    began work on dice game

--- dice_game.py
+++ dice_game.py
@@ -5,7 +5,6 @@ A simple dice game where the player rolls against the computer.
 
 Inputs:
 - user name (str)
-- number of rounds (int)
 
 Processes:
 - import random
@@ -18,4 +17,18 @@ Outputs:
 - current scoreboard
 - roll again?
 """
+import random
+
+def dice_roll():
+    return random.choice([1,2,3,4,5,6])
+
+
+user_name = input("User name: ")
+
+roll_again = "yes"
+while roll_again == "yes":
+    print(f"You rolled a {dice_roll}")
+    roll_again = input("Do you want to roll again? (yes/no) ")
+
+
 
