57f92c717475b3377f3d1191771224b44c90b3e2
Jeremy Eldredge <jeldred1@byu.edu>
2026-05-19 12:06:40 -0600

    created IPO comment blocks

---
+++ dice_game.py
@@ -0,0 +1,21 @@
+"""
+IS 303 A04 - Jeremy Eldredge
+
+A simple dice game where the player rolls against the computer.
+
+Inputs:
+- user name (str)
+- number of rounds (int)
+
+Processes:
+- import random
+- dice_roll: random number from 1 to 6
+- compare_scores: compares user score to computer score, returns winner
+- scoreboard: accumulates total scores for user and computer across all rounds
+
+Outputs:
+- current round results
+- current scoreboard
+- roll again?
+"""
+
---
+++ number_analyzer.py
@@ -0,0 +1,18 @@
+"""
+IS 303 A04 - Jeremy Eldredge
+
+Collects numbers and produces statistical analysis (mean, min, max, range, count above average)
+
+Inputs:
+- user name (str)
+- list of numbers (int)
+
+Processes:
+- import math
+- calculate mean, mix, max, range, count above average (functions)
+
+Outputs:
+- user name
+- list of numbers
+- calculations
+"""
